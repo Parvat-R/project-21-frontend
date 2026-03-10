f2d212009eecda509941b201499887d45adfec22
Surya Pratap Singh <suryapratapsingh@jmangroup.com>
2026-03-10 12:27:39 +0530

    Auth in the SIdebar

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { signIn } from "@/services/operations/Auth";
-import { sign } from "crypto";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
